b47b65e9b64466016e7444fc56c9cf6d17774da7
wffg <15106076885@163.com>
2017-11-22 14:30:13 +0800

    update .gitignore

--- App/Runtime/Temp/a181a603769c1f98ad927e7367c7aa51.php
+++
@@ -1,3 +0,0 @@
-<?php
-//000000000000a:8:{s:8:"sitename";s:6:"来乐";s:5:"title";s:6:"来乐";s:8:"keywords";s:6:"来乐";s:11:"description";s:6:"来乐";s:6:"footer";s:12:"2017©来乐";s:14:"footer-address";s:120:"地址：佛山市南海区里广路2号来乐（地铁六号线浔峰岗站A出口） 加盟咨询热线：400-1883-577";s:16:"water_image_path";s:0:"";s:8:"position";s:1:"1";}
-?>
--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-//phpinfo();
-$redis = new \Redis();
-$redis->connect('redis.duapp.com');
-var_dump($redis);
-?>
