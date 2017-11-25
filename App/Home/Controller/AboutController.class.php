4c545515053800b000c7754aad4cdd2d65a836b7
wffggithub <15106076885@163.com>
2017-11-25 20:28:48 +0800

    a
    a

--- App/Home/Controller/AboutController.class.php
+++ App/Home/Controller/AboutController.class.php
@@ -26,6 +26,7 @@ class AboutController extends ComController {
 
         $all = M('category')->field('id,name')->where(['pid'=>$Pcate['id']])->select();
         $sid = I('id',0,'intval') == 0 ? $all[0]['id'] : I('id',0,'intval');
+         $this->assign('sid',$sid);
         $info = M('about')->where(['sid'=>$sid])->find();
         $this->assign('name',$Pcate['name']);
         $this->assign('all',$all);
